4e894ea63bcbcf755d2b2aea61f30cfbd303f58a
Prateek Sharma <prateek.oct04@gmail.com>
2024-07-31 17:27:02 +0530

    closing-menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,16 @@
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+    sidebar.style.right = '0'
 }
 
 function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+    sidebar.style.right = '-100%'
 }
+
+window.addEventListener('mouseup', function(event) {
+    const sidebar = document.querySelector('.sidebar');
+    if (event.target != sidebar) {
+        sidebar.style.right = '-100%';
+    }
+})
