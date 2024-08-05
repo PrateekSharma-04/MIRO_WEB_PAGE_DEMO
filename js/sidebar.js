81a33e6533776e2a56156636fc985958bf760613
Prateek Sharma <prateek.oct04@gmail.com>
2024-08-05 14:25:09 +0530

    final changes

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,24 @@
-function showSidebar(){
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.height = "55px";
+    } else {
+        document.getElementById("header").style.height = "90px";
+    }
+}
+
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.right = '0'
 }
 
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.right = '-100%'
 }
 
-window.addEventListener('mouseup', function(event) {
+window.addEventListener('mouseup', function (event) {
     const sidebar = document.querySelector('.sidebar');
     if (event.target != sidebar) {
         sidebar.style.right = '-100%';
